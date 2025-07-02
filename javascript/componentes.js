d97e7e9275e4e34a9db40ba478734595c2996d98
Gabriela Nomberto Ramos <U202113876@upc.edu.pe>
2025-07-01 23:36:38 -0500

    fix: router

--- javascript/componentes.js
+++ javascript/componentes.js
@@ -9,8 +9,8 @@ function includeHTML(selector, file) {
 document.addEventListener("DOMContentLoaded", function () {
   const isIndex = path === "/" || path.endsWith("/index.html") || path === "/GrupoC.github.io/" || path.endsWith("/GrupoC.github.io/index.html");
 
-  const headerPath = isIndex ? "paginas/header.html" : "header.html";
-  const footerPath = isIndex ? "paginas/footer.html" : "footer.html";
+  const headerPath = isIndex ? "paginas/header.html" : "../paginas/header.html";
+  const footerPath = isIndex ? "paginas/footer.html" : "../paginas/footer.html";
 
   includeHTML("header", headerPath);
   includeHTML("footer", footerPath);
